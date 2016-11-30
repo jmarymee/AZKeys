ad1d7567c36e89219774d48252121793970553f9
JD Marymee <jd@tech-innovators.com>
2016-11-29 18:58:44 -0800

    Initial Commit

---
+++ AZKeys/AZKeysClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AZKeys
+{
+    public class AZKeysClass
+    {
+        public void StoreKeys(AZKeys secrets, string filePath)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(AZKeys));
+            TextWriter writer = new StreamWriter(filePath);
+
+            ser.Serialize(writer, secrets);
+            writer.Close();
+        }
+
+        public AZKeys GetSecrets(string filePath)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(AZKeys));
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+            AZKeys keys = (AZKeys)ser.Deserialize(stream);
+            return keys;
+        }
+    }
+
+    [Serializable]
+    public class AZKeys
+    {
+        public string azSubscriptionFriendlyName { get; set; }
+        public string azSubscription { get; set; }
+        public string azStorageAccountName { get; set; }
+        public string azStoragePrimaryKey { get; set; }
+        public string azStorageSecondaryKey { get; set; }
+        public string azStorageBaseURI { get; set; }
+        public string SAS { get; set; }
+    }
+}
---
+++ AZKeysConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AZKeys;
+
+namespace AZKeysConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string storePath = @"C:\tools\keys.xml";
+            AZKeys.AZKeys keys = new AZKeys.AZKeys()
+            { azStorageAccountName = "unityscores", azStoragePrimaryKey = "qevOBB7bEvX69HG18vROe1rQLjZM7ZfLXuW9aOpmAgHnq/XMyuBx/fdagQ4ytWISaksMVKphMlICfPho8/CUIg==" };
+
+            AZKeysClass az = new AZKeysClass();
+            az.StoreKeys(keys, storePath);
+
+            AZKeys.AZKeys key2 = az.GetSecrets(storePath);
+
+        }
+    }
+}
